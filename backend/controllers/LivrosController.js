e85a811892d5bc6ef9229e92b65664ef23c44cba
moraeseduarda <eduardamorads@gmail.com>
2025-11-09 11:01:59 -0300

    feat: implementa controle de livros e avaliações com rotas e modelos

---
+++ backend/controllers/LivrosController.js
@@ -0,0 +1,57 @@
+import Livro from '../models/Livro.js'
+
+const registraLivro = async (req, res) => {
+    const { titulo, autor, imagemCapa, nota } = req.body;
+
+    try {
+        const livroExiste = await Livro.findById({ id });
+
+        if (livroExiste) {
+            return res.status(400).jspn({message: 'Já existe um usuário com esse ID.'});
+        }
+
+        const livro = await Livro.create({
+            titulo,
+            autor,
+            imagemCapa,
+            nota,
+        });
+
+        if (livro) {
+            res.status(201).json({
+                titulo,
+                autor,
+                imagemCapa,
+                nota,
+            });
+        } else {
+            res.status(400).json({message: 'Dados do livro inválidos.'});
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Erro interno do sevidor.', erro: error.message});
+    }
+};
+
+const pegaLivroPorId = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const livro = await Livro.findById({ id });
+
+        if (livro) {
+            return res.status(200).jspn(livro);
+        } else {
+            res.status(404).json({msg: 'Livro não encontrado.'});
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Erro interno do sevidor.', erro: error.message});
+    }
+};
+
+export { registraLivro, pegaLivroPorId };
