5d2cff8ff68007b340fefbd372d5a44a59d3ee10
Isabela <isabela.serafim@grad.ufsc.br>
2025-11-25 02:29:18 -0300

    Arquivos que não estavam mais sendo usados

--- backend/data/comunidades.js
+++
@@ -1,6 +0,0 @@
-export const comunidadesLiterarias = [
-  { nome: "Ficção Científica & Cyberpunk", descricao: "Para amantes de distopias e robôs." },
-  { nome: "Clube dos Clássicos Brasileiros", descricao: "Leitura de Machado e Clarice." },
-  { nome: "Mistério e Crime Real", descricao: "Debates sobre thrillers e true crime." },
-  { nome: "Fantasia Medieval", descricao: "Dragões, magia e Tolkien." },
-];
